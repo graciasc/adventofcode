080e08a52ae5adfe6f35b3c1f9fc1971e8b84123
gracias <gracias.claude@outlook.com>
2022-12-04 19:54:42 -0700

    time complexity : o(n + n) = o (n) space complexity : o( n )

--- day-one/index.js
+++ day-one/index.js
@@ -1,73 +1,24 @@
-// import { readfileSync } from 'fs'
-// import * as fs from 'fs'
-// import * as path from 'path'
-// const file = (fileName): string => (path.join(__dirname, fileName))
-// const fileInputStr: string = fs.readFileSync(file('input.txt'), 'utf8')
-var fileInputStr = "12 56\n12 48";
-// 68 60
-var maxNumber = 0;
-var total = 0;
-var firstNumber = '';
-var secondNumber = '';
-for (var i = 0; i < fileInputStr.length; i++) {
-    if (total > maxNumber) {
-        maxNumber = total;
-    }
-    if (fileInputStr[i] === '\n') {
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var path = require("path");
+var file = function (fileName) { return (path.join(__dirname, fileName)); };
+var fileInputStr = fs.readFileSync(file('input.txt'), 'utf8');
+var allInputs = fileInputStr.split('\n');
+var elfCount = 0;
+var totalNumber = 0;
+var elfMax = -Infinity;
+var elf = 0;
+for (var i = 0; i < allInputs.length; i++) {
+    if (allInputs[i] === '') {
+        if (totalNumber > elfMax) {
+            elfMax = totalNumber;
+            elf = elfCount;
+        }
+        elfCount += 1;
+        totalNumber = 0;
         continue;
     }
-    console.log(fileInputStr[i]);
-    console.log('secondNumber', secondNumber);
-    console.log('firstNumber', firstNumber);
-    if (secondNumber === '') {
-        firstNumber += fileInputStr[i];
-    }
-    else if (firstNumber !== '') {
-        secondNumber += fileInputStr[i];
-    }
-    else {
-        total = parseInt(firstNumber) + parseInt(secondNumber);
-        firstNumber = '';
-        secondNumber = '';
-    }
+    totalNumber += parseInt(allInputs[i]);
 }
-console.log(maxNumber);
-// const allInputs = fileInputStr.split('\n')
-//
-// const formattedInputs: number[][] | undefined[] = []
-// let elfCount: number = 0
-//
-// for (let i = 0; i < allInputs.length; i++) {
-//   if (allInputs[i] === '') {
-//     elfCount++
-//     continue
-//   }
-//
-//   formattedInputs[elfCount] = formattedInputs[elfCount] === undefined ? [] : formattedInputs[elfCount]
-//
-//   formattedInputs[elfCount].push(parseInt((allInputs[i])))
-// }
-//
-// console.log('elf count', elfCount, JSON.stringify(formattedInputs))
-//
-// // check the highest total calories
-//
-// const maxElf = {
-//   totalCount: 0,
-//   elf: -1
-// }
-// for (let i = 0; i < formattedInputs.length; i++) {
-//   const elfCaloriesInfo: number[] = formattedInputs[i]
-//   let elfCaloriesTotal = 0
-//
-//   for (let j = 0; j < elfCaloriesInfo.length; j++) {
-//     // TODO: fix type from string to number
-//     elfCaloriesTotal += elfCaloriesInfo[j]
-//   }
-//
-//   if (elfCaloriesTotal > maxElf.totalCount) {
-//     maxElf.totalCount = elfCaloriesTotal
-//     maxElf.elf = i
-//   }
-// }
-// console.log('biggest calories elf', maxElf)
+console.log(elfMax, elf);
--- day-one/index.ts
+++ day-one/index.ts
@@ -1,4 +1,3 @@
-import { readfileSync } from 'fs'
 import * as fs from 'fs'
 import * as path from 'path'
 
@@ -7,40 +6,16 @@ const file = (fileName): string => (path.join(__dirname, fileName))
 const fileInputStr: string = fs.readFileSync(file('input.txt'), 'utf8')
 const allInputs = fileInputStr.split('\n')
 
-const formattedInputs: number[][] | undefined[] = []
-let elfCount: number = 0
+let totalNumber = 0
+let elfMax = -Infinity
 
 for (let i = 0; i < allInputs.length; i++) {
   if (allInputs[i] === '') {
-    elfCount++
+    elfMax = Math.max(elfMax, totalNumber)
+    totalNumber = 0
     continue
   }
-
-  formattedInputs[elfCount] = formattedInputs[elfCount] === undefined ? [] : formattedInputs[elfCount]
-
-  formattedInputs[elfCount].push(parseInt((allInputs[i])))
+  totalNumber += parseInt(allInputs[i])
 }
 
-console.log('elf count', elfCount, JSON.stringify(formattedInputs))
-
-// check the highest total calories
-
-const maxElf = {
-  totalCount: 0,
-  elf: -1
-}
-for (let i = 0; i < formattedInputs.length; i++) {
-  const elfCaloriesInfo: number[] = formattedInputs[i]
-  let elfCaloriesTotal = 0
-
-  for (let j = 0; j < elfCaloriesInfo.length; j++) {
-    // TODO: fix type from string to number
-    elfCaloriesTotal += elfCaloriesInfo[j]
-  }
-
-  if (elfCaloriesTotal > maxElf.totalCount) {
-    maxElf.totalCount = elfCaloriesTotal
-    maxElf.elf = i
-  }
-}
-console.log('biggest calories elf', maxElf)
+console.log(elfMax)
